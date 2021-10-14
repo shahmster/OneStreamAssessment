4819b0add8bf8b1f58804b154d768a78f239fa70
mohammed.shah <shahmster@gmail.com>
2021-10-14 19:39:16 +0200

    commited bin files

---
+++ LeadsApi/Leads.Api/Persistence/EntityConfigurations/WebMethodMap.cs
@@ -0,0 +1,23 @@
+﻿using Leads.Api.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leads.Api.Persistence.EntityConfigurations
+{
+    public class WebMethodMap : IEntityTypeConfiguration<WebMethod>
+    {
+        public void Configure(EntityTypeBuilder<WebMethod> builder)
+        {
+            builder.ToTable("TR_WebMethod");
+            builder.HasKey(key => key.WebMethodId);
+
+          
+
+        }
+    }
+
+}
